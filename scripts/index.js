9670d18fe43ccbc0758996ae65c8c44de6a3489e
Mihai Lita <lita.mihai@gmail.com>
2017-07-27 14:37:54 +0300

    Added background image. Added countdown added subscription box Added social media icons(no functionality yet)

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+var targetDate = new Date("Sep 1, 2017 00:00:00").getTime();
+
+var x = setInterval(function() {
+    var currentDate = new Date().getTime();
+
+    var remainingTime = targetDate - currentDate;
+
+    var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+    document.getElementById('timer-days').innerHTML = days;
+    document.getElementById('timer-hours').innerHTML = hours;
+    document.getElementById('timer-minutes').innerHTML = minutes;
+    document.getElementById('timer-seconds').innerHTML = seconds;
+
+}, 1000);
